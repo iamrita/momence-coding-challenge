46aed19a10896451968e439cdbd4bf017692671b
Amrita <amritav.ca@gmail.com>
2023-03-29 15:17:59 -0700

    downloaded react query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
+
+const fetchData = async() => {
+  const res = await fetch("https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt")
+  console.log(res.text)
+  return res.text
+}
 function App() {
+  const [exchange, showExchange] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      showExchange(true);
+      const data = fetchData()
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown); // avoid memory leaks 
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="title">
+        <h1>Welcome!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          So I heard you're planning a trip to Prague. Exciting! This is a
+          website for <b>Czech Currency Exchange</b>. Take a look at the
+          information below, to check out what the exchange rates are for
+          various countries. Feel free to use the currency calculator below for
+          more refined searching.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Press any key to begin.</p>
+      </div>
+      {exchange ? <div className="title"> Exchange </div> : <></>}
     </div>
   );
 }
