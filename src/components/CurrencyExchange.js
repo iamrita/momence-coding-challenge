833b9f1f38418ac23bc2be3770d467c06cbfab35
Amrita <amritav.ca@gmail.com>
2023-03-31 14:43:11 -0700

    basic functionality done, now for styling

--- src/components/CurrencyExchange.js
+++ src/components/CurrencyExchange.js
@@ -1,30 +1,31 @@
-import {useState, React} from "react";
+import { useState, React } from "react";
 import Select from "react-select";
 
 function CurrencyExchange(props) {
   const [selectedOption, setSelectedOption] = useState("");
-  const [currAmount, setCurrAmount] = useState("")
-  const [finalAmount, setFinalAmount] = useState("")
-  const [clicked, setClicked] = useState(false)
+  const [currAmount, setCurrAmount] = useState("");
+  const [finalAmount, setFinalAmount] = useState("");
+  const [clicked, setClicked] = useState(false);
 
-  let options = [] 
+  let options = [];
   for (let i = 0; i < props.currencies.length; i++) {
-    options.push({value: props.currencies[i].country, label: props.currencies[i].country})
-  } // convert options for dropdown menu 
-
+    options.push({
+      value: props.currencies[i].country,
+      label: props.currencies[i].country,
+    });
+  } // convert options for dropdown menu
 
   function calculate(currObj) {
-    setClicked(true)
-    const country = currObj.country
-    const exchangeRate = props.currencies.find(c => c.country === country)
-    console.log(exchangeRate)
-    let exchangeAmount = exchangeRate.amount 
-    let rate = exchangeRate.rate
-    let floatAmt = parseFloat(currObj.amt)
-    console.log(props.currencies)
-    setFinalAmount(String((floatAmt * exchangeAmount)/rate))
-
-}
+    setClicked(true);
+    const country = currObj.country;
+    const exchangeRate = props.currencies.find((c) => c.country === country);
+    console.log(exchangeRate);
+    let exchangeAmount = exchangeRate.amount;
+    let rate = exchangeRate.rate;
+    let floatAmt = parseFloat(currObj.amt);
+    console.log(props.currencies);
+    setFinalAmount(String((floatAmt * exchangeAmount) / rate));
+  }
   return (
     <div>
       <Select
@@ -32,14 +33,28 @@ function CurrencyExchange(props) {
         onChange={setSelectedOption}
         options={options}
       />
-       <label>
-        Enter amount in CZK: <input value={currAmount} onChange={(e) => {
-            setClicked(false)
-            setCurrAmount(e.target.value)
-        }} name="myInput" />
+      <label>
+        Enter amount in CZK:
+        <input
+          value={currAmount}
+          onChange={(e) => setCurrAmount(e.target.value)}
+          name="myInput"
+        />
       </label>
-      <button onClick={(e) => calculate({country: selectedOption.value, amt: currAmount})}>Go</button>
-      {clicked ? <div>Total in {selectedOption.value} is {finalAmount}</div> : <></>}
+      <button
+        onClick={(e) =>
+          calculate({ country: selectedOption.value, amt: currAmount })
+        }
+      >
+        Go
+      </button>
+      {clicked ? (
+        <div>
+          Total in {selectedOption.value} is {finalAmount}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
